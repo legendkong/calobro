03b77a09985e004b6e8ce8721211346a30170e5e
Jonathan Kong <jonathankong1996@gmail.com>
2024-03-08 00:56:50 +0800

    add react query

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import './globals.css';
+import QueryProvider from '@/components/query-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,14 +15,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
---
+++ components/query-provider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import React, { ReactNode, useState } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// whenever fetch data, go into cache, if want to refresh cache, either invalid query or refresh the page
+// since staltTime is infinity, it will never refetch data
+export default function QueryProvider({ children }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
