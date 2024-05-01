152124a66f07bc8023fb678e42f47d3a04107fa5
Jonathan Kong <jonathankong1996@gmail.com>
2024-05-01 17:05:53 +0800

    comment out `/test` page

--- app/test/page.js
+++ app/test/page.js
@@ -1,34 +1,34 @@
-import OpenAI from 'openai';
-import { createInterface } from 'readline';
+// import OpenAI from 'openai';
+// import { createInterface } from 'readline';
 
-const readline = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const readline = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-async function askFoodAndFetchNutrition() {
-  const openai = new OpenAI(process.env.OPENAI_API_KEY);
+// async function askFoodAndFetchNutrition() {
+//   const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
-  readline.question('Enter the food item: ', async (food) => {
-    const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
-          content:
-            'You are a helpful assistant designed to output JSON. Give the nutrition information for the following food in Singapore. Give your answer in calories, fat, protein, and carbs.',
-        },
-        {
-          role: 'user',
-          content: `This is the food: ${food}`,
-        },
-      ],
-    });
+//   readline.question('Enter the food item: ', async (food) => {
+//     const completion = await openai.chat.completions.create({
+//       model: 'gpt-3.5-turbo',
+//       messages: [
+//         {
+//           role: 'system',
+//           content:
+//             'You are a helpful assistant designed to output JSON. Give the nutrition information for the following food in Singapore. Give your answer in calories, fat, protein, and carbs.',
+//         },
+//         {
+//           role: 'user',
+//           content: `This is the food: ${food}`,
+//         },
+//       ],
+//     });
 
-    console.log(completion.choices[0].message.content);
+//     console.log(completion.choices[0].message.content);
 
-    readline.close();
-  });
-}
+//     readline.close();
+//   });
+// }
 
-askFoodAndFetchNutrition();
+// askFoodAndFetchNutrition();
